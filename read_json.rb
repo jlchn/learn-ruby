49540d4e7e1605a4fa7c762f2162672a100fda20
jiangli <jianglichn@163.com>
2019-03-13 19:31:24 +0800

    add array to file

--- read_json.rb
+++ read_json.rb
@@ -5,12 +5,14 @@ lines = Array.new
 File.open("/home/jiangli/Documents/users.txt").each { |line| lines << line.strip }
 
 result = Hash.new
+result2 = Array.new
 datas.each do |d|
 	lines.each do |l|
 		if l.empty?
 			next
 		end
 		if d['email'] == l
+			 result2 << d['email'] + ";" + d['section1'] + ";" + d['section2'] + ";" + d['section3'] + "\n"
                         section = d['section1'].split(/Div./).first
 			if result.key?(section)
 				result[section] += 1;
@@ -22,3 +24,7 @@ datas.each do |d|
 end
 
 puts result
+
+File.open("output.csv",'w') { |file|
+        file.puts(result2)
+}
